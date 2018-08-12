ac9d98422078b410b42bf804698f88fad60daeac
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-12 13:04:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+adds = (a, b) => a + b
+
+subtracts = (a, b) => a - b
+
+multiply = (a, b) => a * b
+
+divide = (a, b) => a / b
+
+inc = (a) => a++
+
+dec = (a) => a--
+
+makeInt = (n) => parseInt(n, 10)
+
+preserveDecimal = (n) => parseFloat(n)
